fd3e1d1de2a2d4d6a0700ce6f1f4fbcaef5f6fad
carlosyanez2009 <carlosyanez2009@gmail.com>
2021-07-12 20:53:14 -0500

    menu navigation

--- frontEnd/esm-app/src/pages/Menu.js
+++ frontEnd/esm-app/src/pages/Menu.js
@@ -1,7 +1,41 @@
 import React from 'react';
-function Menu(){
-    return (
-        <div></div>
-    );
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/core/styles';
+import Login from'../pages/Login';
+import {NavLink} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import Routes from '../routes/Routes';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export default function Menu() {
+  const classes = useStyles();
+
+  return (
+    <div>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <MenuList>
+          <MenuItem>Registro de Clientes</MenuItem>
+          <MenuItem>Registro de Productos</MenuItem>
+          <MenuItem>Registro de Ventas</MenuItem>
+          <MenuItem>Registro de Usuarios</MenuItem>
+          <MenuItem>Registro de Asignaciones</MenuItem>
+          <MenuItem>Agenda</MenuItem>
+          <MenuItem ><NavLink to="/" activeClassName="active" >Salir</NavLink></MenuItem>
+        </MenuList>
+      </Paper>
+      <div>
+      </div>
+    </div>
+    </div>
+  );
 }
-export default Menu;
--- frontEnd/esm-app/src/routes/Routes.js
+++ frontEnd/esm-app/src/routes/Routes.js
@@ -2,15 +2,15 @@ import {React} from 'react';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
 import Login from'../pages/Login';
 import Menu from'../pages/Menu';
-//import AddClients from'../pages/AddClients';
-//            <Route path="/menu" component={<Menu />}/>
-//<Route path="/addClients" component={<AddClients  />}/>-
 function Routes(){
     return (
         <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Login} />
             <Route exact path="/menu" component={Menu} />
+            <Route path="*">
+                <h3>Pagina no encontrada</h3>
+            </Route>
         </Switch>
         </BrowserRouter>
     );
